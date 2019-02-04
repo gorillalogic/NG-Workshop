2baacc0be21d81985aaec904662989711c5ce8ca
Gerardo Lopez Falcon <gerardo.lopez@gorillalogic.com>
2019-02-04 17:26:17 -0600

    Add subscriptions

--- src/pokedex/features/components/pokemon-details/pokemon-details.component.ts
+++ src/pokedex/features/components/pokemon-details/pokemon-details.component.ts
@@ -1,29 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PokemonService } from '../../../core/services/pokemon.service';
 import { switchMap, filter } from 'rxjs/operators';
 import { Pokemon } from '@pokedex/core/models/pokemon.interfaces';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-pokemon-details',
   templateUrl: './pokemon-details.component.html',
   styleUrls: ['./pokemon-details.component.scss']
 })
-export class PokemonDetailsComponent implements OnInit {
+export class PokemonDetailsComponent implements OnInit, OnDestroy {
   pokemonDetail: Pokemon;
+  private subscription: Subscription;
   constructor(private route: ActivatedRoute, private pokemonService: PokemonService) {}
 
   ngOnInit() {
-    this.route.paramMap
-      .pipe(
-        switchMap(params => {
-          const pokemonId = Number(params.get('id'));
-          return this.pokemonService
-            .getPokemons()
-            .pipe(switchMap(pokemons => pokemons.filter(pokemon => pokemon.id === pokemonId)));
-        })
-      )
-      .subscribe(pokemon => (this.pokemonDetail = pokemon));
+    this.subscription = new Subscription();
+    this.subscription.add(
+      this.route.paramMap
+        .pipe(
+          switchMap(params => {
+            const pokemonId = Number(params.get('id'));
+            return this.pokemonService
+              .getPokemons()
+              .pipe(switchMap(pokemons => pokemons.filter(pokemon => pokemon.id === pokemonId)));
+          })
+        )
+        .subscribe(pokemon => (this.pokemonDetail = pokemon))
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
--- src/pokedex/features/components/pokemon-list/pokemon-list.component.ts
+++ src/pokedex/features/components/pokemon-list/pokemon-list.component.ts
@@ -1,19 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Pokemon } from '@pokedex/core/models/pokemon.interfaces';
 import { PokemonService } from '@pokedex/core//services/pokemon.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
   styleUrls: ['./pokemon-list.component.scss']
 })
-export class PokemonListComponent implements OnInit {
+export class PokemonListComponent implements OnInit, OnDestroy {
+  private subscription: Subscription;
   constructor(private pokemonService: PokemonService) {}
   pokemons: Pokemon[];
 
   ngOnInit() {
-    this.pokemonService.getPokemons().subscribe(info => {
-      this.pokemons = info;
-    });
+    this.subscription = new Subscription();
+    this.subscription.add(
+      this.pokemonService.getPokemons().subscribe(info => {
+        this.pokemons = info;
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
