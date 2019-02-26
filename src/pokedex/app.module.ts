87a36c4893ad802099f4bcd0f0056212b5e8c3be
Gerardo Lopez Falcon <gerardo.lopez@gorillalogic.com>
2019-02-26 13:01:40 -0600

    Add app module

--- src/pokedex/app.module.ts
+++ src/pokedex/app.module.ts
@@ -9,6 +9,11 @@ import { SharedModule } from '@pokedex/shared/shared.module';
 import { LayoutModule } from '@pokedex/layout/layout.module';
 import { PokemonModule } from '@pokedex/features/components/pokemon.module';
 import { HttpModule } from '@angular/http';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+import { reducers } from '@pokedex/index.reducers';
+import { effects } from '@pokedex/index.effects';
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,8 +23,13 @@ import { HttpModule } from '@angular/http';
     HttpClientModule,
     CoreModule.forRoot(),
     LayoutModule,
+    StoreModule.forRoot(reducers),
+    EffectsModule.forRoot(effects),
     SharedModule,
-    PokemonModule
+    PokemonModule,
+    StoreDevtoolsModule.instrument({
+      maxAge: 25
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
