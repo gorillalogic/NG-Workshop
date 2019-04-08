3e0a931a47d2502e53fd9094e7a5ea92624599cf
Mariano Alvarez <mariano.alvarez@gorillalogic.com>
2019-04-07 18:22:50 -0600

    preloading strategy  example#3

--- bp/src/app/app-routing.module.ts
+++ bp/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-
+  { path: 'lazy-loading', loadChildren: '../examples/02-03-04-lazy-loading/lazy/lazy-loading.module#LazyLoadingModule' }
 ];
 
 @NgModule({
@@ -38,5 +38,13 @@ export class AppRoutingModule { }
 
 
 
-// Solution:
+// Solution example# 2:
 // { path: 'lazy-loading', loadChildren: '../examples/02-03-04-lazy-loading/lazy/lazy-loading.module#LazyLoadingModule' }
+
+
+// Solution example# 3
+// { path: 'lazy-loading', loadChildren: '../examples/02-03-04-lazy-loading/lazy/lazy-loading.module#LazyLoadingModule',
+// data: { preload: true } }
+
+
+// { preloadingStrategy: SelectivePreloadingStrategyService }
